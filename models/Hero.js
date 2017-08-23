56845f338f7b465e25e2974cb07a15eadd22debd
Evan Coakley <evancoakley1@gmail.com>
2017-08-23 15:35:45 -0500

    updates

--- models/Hero.js
+++ models/Hero.js
@@ -5,13 +5,18 @@ const HeroSchema = new Schema({
     heroName: String,
     comicUniverse: String,
     powers: {
-        firstPower: String,
-        secondPower: String
+        power1: String,
+        power2: String,
+
+        origin: String,
+        enum: ["super-human", "above-average", "average", "below-average"]
+
     },
     yearsActive: Number,
     associates: {
-        closeAss1: String,
-        closeAss2: String
+        closeAssoc1: String,
+        closeAssoc2: String,
+
     },
     nemeses: {
         nemesis1: String,
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bluebird = require("bluebird");
 const bodyparser = require("body-parser");
 const logger = require("morgan");
-const Comic = require("./models/Hero");
+const Hero = require("./models/Hero");
 
 const app = express();
 mongoose.Promise = bluebird;
@@ -27,6 +27,30 @@ app.post("/comichero", function (req, res) {
 });
 app.get("/comichero", function (req, res) {
     Hero.find()
-        .then(function (foundComic))
+        .then(function (foundHero) {
+            if (!foundComic) {
+                return res.send({ msg: "No Hero Found" })
+            }
+            res.send(foundComics)
+        })
+        .catch(function (err) {
+            res.status(500).send(err);
+        });
 
+});
+
+app.get("/comichero/:id", function (req, res) {
+    Hero.findById(req.params.id)
+        .then(function (foundHero) {
+            if (!foundBook) {
+                return res.send({ msg: "No Hero Found" });
+            }
+            res.send(foundHero);
+        })
 })
+
+
+
+
+
+app.listen(8000, () => console.log("Server running on port 8000!"));
